a8d211de7e3a67d2c7eebcfa05cb4a9942671ce3
s3506901 <s3485566@student.rmit.edu.au>
2018-05-08 23:02:49 +0000

    booking implementation update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,14 @@
 class BookingsController < ApplicationController
+  require "date"
+  Time.zone = "Australia/Melbourne"
   def new
     @booking = Booking.new
   end
   def booking_params
     params.require(:booking).permit(:users_id, :cars_id, :user_firstname, :user_lastname,
                                         :user_email, :user_phone, :user_phone, :car_make,
-                                        :car_model, :car_year, :car_size, :car_price)
+                                        :car_model, :car_year, :car_size, :car_price, :pickup, 
+                                        :return)
   end
   def create
     car_id = booking_params[:car_id]
@@ -15,6 +18,66 @@ class BookingsController < ApplicationController
       redirect_to new_booking_path(:car_id => car_id)
       return
     end
+    unless Car.find(car_id).status == "Available"
+      flash[:danger] = "This car is not available."
+      redirect_to cars_path
+      return
+    end
     user = User.find(user_id)
+    if user.available
+      @booking = Booking.new(booking_params)
+      if @booking.save
+        flash[:success] = 'Booking was successfully created.'
+        # UserMailer.welcome_email(user).deliver
+        PickupCheckJob.set(wait_until: @booking.checkOutTime + 30.minutes).perform_later(@booking.id)
+        redirect_to @booking
+        car = Car.find(@booking.car_id)
+        car.update_attribute(:status, "Reserved")
+        user.update_attribute(:available, false)
+      else
+        if @booking.errors.any?
+          @booking.errors.full_messages.each do |message|
+            flash[:errors] = message
+          end
+        end
+        redirect_to new_booking_path(:car_id => car_id)
+      end
+    else
+      flash[:danger] = "This user have a car in hold, and cannot reserve another car until you return it."
+      redirect_to user
+    end
+  end
+  def pickup
+    @booking = Booking.find(params[:id])
+    if @booking.checkOutTime > Time.now
+      flash[:error] = 'You need to wait to pick up the car!'
+      redirect_to @booking
+    elsif @booking.update_attribute(:pickUpTime, Time.now)
+      flash[:success] = 'Car was successfully picked up. Have a good time!'
+      @booking.update_attribute(:bookingStatus, "Active")
+      car = Car.find(@booking.car_id)
+      car.update_attribute(:status, "CheckedOut")
+      ReturnCheckJob.set(wait_until: @booking.expectedReturnTime + 60).perform_later(@booking.id)
+      redirect_to @booking
+    end
+  end
+
+  def returncar
+    @booking = Booking.find(params[:id])
+    if @booking.update_attribute(:returnTime, Time.now)
+      flash[:success] = 'Car was successfully returned. Thank you!'
+      @booking.update_attribute(:bookingStatus, "Complete")
+      user = User.find(@booking.user_id)
+      car = Car.find(@booking.car_id)
+      car.update_attribute(:status, "Available")
+      
+      price = car.hourlyRentalRate
+      hold_time = (@booking.returnTime - @booking.pickUpTime)/3600.0 # convert to hours
+      charge = user.rentalCharge + price*hold_time
+      user.update_attribute(:rentalCharge, charge) 
+      user.update_attribute(:notification, "You can reserve a car now!")
+      user.update_attribute(:available, true)
+      redirect_to @booking
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(first:                   "Root",
+             lastname:               "Admin",
+             email:   "root_admin@email.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             role: "Admin")
+             
+User.create!(first:                   "Root",
+             lastname:               "SuperAdmin",
+             email: "root_superadmin@email.com",
+             password:              "123456",
+             password_confirmation: "123456",
+             role: "SuperAdmin")
+             
+User.create!(name:  "Example Customer",
+             email: "example_customer@email.com",
+             phone:             "1234567890",
+             licenseN:           "123456789",
+             password:              "123456",
+             password_confirmation: "123456",
+             role: "Customer")
+
+99.times do |n|
+  firstname  = Faker::Name.first_name
+  lastname  = Faker::Name.last_name
+  email = "example-#{n+1}@rmit.org"
+  phone =  Faker::Number.number(10)
+  licenseN = Faker::Number.number(9)
+  password = "password"
+  role = "Customer"
+  User.create!(firstname:  firstname,
+               lastname: lastname,
+               email: email,
+               phone: phone,
+               licenseN: licenseN,
+               password:              password,
+               password_confirmation: password, 
+               role: role)
+end
+
+# Generate Car samples:
+50.times do |cnt|
+    make = Faker::Vehicle.manufacture
+    model = "model#{cnt%8}"
+    price = Faker::Number.between(80.00, 300.00)
+    size = "#{Car.styles.key(cnt%3)}"
+    location = Faker::Address.street_address
+    status = "Available"
+    Car.create!(
+        make: make,
+        model: model,
+        price: price,
+        size: size,
+        location: location,
+        status: status)
+end
