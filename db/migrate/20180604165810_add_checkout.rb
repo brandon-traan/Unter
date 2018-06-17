b52e083e99392b0599882571a520a290f7852caa
Brandon Tran <brandontran1993@gmail.com>
2018-06-17 23:40:00 +1000

    Cast dateTime to Date

--- db/migrate/20180604165810_add_checkout.rb
+++ db/migrate/20180604165810_add_checkout.rb
@@ -1,5 +1,5 @@
 class AddCheckout < ActiveRecord::Migration[5.1]
   def change
-    add_column :bookings, :checkOut, :datetime, default: "" ,:null => false
+    add_column :bookings, :checkOut, :datetime, 'USING CAST(case when datetime = '' then null else datetime end AS date)'
   end
 end
