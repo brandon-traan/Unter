fb73a884165770308c390f66a00b26850525e44f
Brandon Tran <brandontran1993@gmail.com>
2018-06-17 23:21:59 +1000

    Change Type float to precision

--- db/migrate/20180524192833_change_price_type.rb
+++ db/migrate/20180524192833_change_price_type.rb
@@ -1,6 +1,6 @@
 class ChangePriceType < ActiveRecord::Migration[5.1]
   def change
-    change_column :cars, :price, :float
-    change_column :bookings, :car_price, :float
+    change_column :cars, :price, USING price::double precision
+    change_column :bookings, :car_price, USING price::double precision
   end
 end
